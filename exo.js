0ee7f9094d9c5e08146f1af30bc0d912dfdb1db6
kevinmr59 <kevinmura59610@gmail.com>
2021-11-24 19:13:13 +0100

    strings 2

--- exo.js
+++ exo.js
@@ -1,3 +1,21 @@
 let pomme = "Elle est belle ma pomme !";
 
+console.log(pomme.lastIndexOf('pomme'));
+
 console.log(pomme.lastIndexOf('m'));
+
+console.log(pomme.startsWith('Elle'));
+
+console.log(pomme.endsWith('!'));
+
+let nouvelleChaine
+nouvelleChaine=pomme.substring(18,21);
+nouvelleChaine.innerHTML="pomme";
+console.log(pomme[0]);
+let ligne =document.getElementById('one-line');
+
+
+for (let i=0; i<pomme.length;i++){
+   ligne.innerHTML+= pomme[i]+'<br>'
+    console.log(pomme [i])
+}
